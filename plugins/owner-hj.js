1409e0063e221bc8a168864cbfd5e2ca862cc421
Ofc.Diego <151096440+OfcDiego@users.noreply.github.com>
2024-06-23 19:02:29 -0500

    Actualizar owner-hj.js

--- plugins/owner-hj.js
+++ plugins/owner-hj.js
@@ -1,4 +1,4 @@
-let handler = (m) => m;
+/*let handler = (m) => m;
 handler.all = async function (m) {
   
 
@@ -6,7 +6,7 @@ handler.all = async function (m) {
     var hj = `𝙲𝚁𝙴𝙰𝙳𝙾𝚁 @${global.suittag} 𝚄𝙽 𝙷𝚄𝙼𝙰𝙽𝙾 𝚃𝙴 𝙰 𝙸𝙽𝚅𝙾𝙲𝙰𝙳𝙾`;
 
     //m.reply(hj, m.chat,{ mentions: conn.parseMention(hj)});
-    sem prefixo
+    sem prefijo
     conn.reply(
       m.chat,
       `𝙲𝚁𝙴𝙰𝙳𝙾𝚁 @${hachejota} 𝚄𝙽 𝙷𝚄𝙼𝙰𝙽𝙾 𝚃𝙴 𝙰 𝙸𝙽𝚅𝙾𝙲𝙰𝙳𝙾`,fliveLoc,
@@ -15,4 +15,4 @@ handler.all = async function (m) {
   }
   return !0;
 };
-export default handler;
+export default handler;*/
