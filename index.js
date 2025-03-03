36c801f73fdc7bdc00b5bf7927e25d5063022bdd
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-03-03 12:08:54 -0300

    reviviendo

--- index.js
+++ index.js
@@ -1,70 +1,80 @@
-console.log('💥 Iniciando...')
-import { join, dirname } from 'path'
-import { createRequire } from 'module'
-import { fileURLToPath } from 'url'
-import { setupMaster, fork } from 'cluster'
-import { watchFile, unwatchFile } from 'fs'
-import cfonts from 'cfonts'
-import { createInterface } from 'readline'
-import yargs from 'yargs'
-import chalk from 'chalk'
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const require = createRequire(__dirname) 
-const { name, description, author, version } = require(join(__dirname, './package.json')) 
-const { say } = cfonts
-const rl = createInterface(process.stdin, process.stdout)
-say('Hachiko\nBot-MD', {
+import { join, dirname } from 'path';
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { setupMaster, fork } from 'cluster';
+import { watchFile, unwatchFile } from 'fs';
+import cfonts from 'cfonts';
+import { createInterface } from 'readline';
+import yargs from 'yargs';
+import chalk from 'chalk';
+console.log('\n✰ Iniciando Hachiko ✰');
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(__dirname);
+const { name, description, author, version } = require(join(__dirname, './package.json'));
+const { say } = cfonts;
+const rl = createInterface(process.stdin, process.stdout);
+say('Hachiko\nBot', {
 font: 'block',
 align: 'center',
-colors: ['white']
-})
-say(`MultiDevice`, {
+colors: ['red']
+});
+say(`Multi Device`, {
 font: 'chrome',
 align: 'center',
-colors: ['red', 'white']
-})
-console.log(chalk.green(`Bot: ${name}\Autor: HacheJota\nColaborador Estrella: OfcDiego`))
-var isRunning = false
+colors: ['red']
+});
+say(`Developed By • Hachejota`, {
+font: 'console',
+align: 'center',
+colors: ['yellow']
+});
+var isRunning = false;
 function start(file) {
-if (isRunning) return
-isRunning = true
-let args = [join(__dirname, file), ...process.argv.slice(2)]
+if (isRunning) return;
+isRunning = true;
+let args = [join(__dirname, file), ...process.argv.slice(2)];
 say([process.argv[0], ...args].join(' '), {
 font: 'console',
 align: 'center',
 colors: ['green']
-})
+});
 setupMaster({
 exec: args[0],
 args: args.slice(1),
-})
-let p = fork()
+});
+let p = fork();
 p.on('message', data => {
 switch (data) {
 case 'reset':
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-break
+p.process.kill();
+isRunning = false;
+start.apply(this, arguments);
+break;
 case 'uptime':
-p.send(process.uptime())
-break
+p.send(process.uptime());
+break;
 }
-})
+});
 p.on('exit', (_, code) => {
-isRunning = false
-console.error('⚙️ Error:\n', code)
+isRunning = false;
+console.error('🚩 Error:\n', code);
 process.exit();
-if (code === 0) return
+if (code === 0) return;
 watchFile(args[0], () => {
-unwatchFile(args[0])
-start(file)
-})
-})
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+unwatchFile(args[0]);
+start(file);
+});
+});
+let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
 if (!opts['test'])
 if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim())
-})
+p.emit('message', line.trim());
+});
 }
-start('main.js')
+process.on('warning', (warning) => {
+if (warning.name === 'MaxListenersExceededWarning') {
+console.warn('🚩 Se excedió el límite de Listeners en:');
+console.warn(warning.stack);
+}
+});
+start('main.js');
