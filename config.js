de769ca5297e18c3a4017233c660fbaa4faba5ec
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-03-03 15:32:28 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import chalk from 'chalk'
 import fs from 'fs'
 import moment from 'moment-timezone'
 import path, { join } from 'path'
-import { getRandomImageBoys } from 'module-gatadios'
+//import { getRandomImageBoys } from 'module-gatadios'
 import { fileURLToPath, pathToFileURL } from 'url'
 import { platform } from 'process'
 global.__filename = function filename(pathURL = import.meta.url, rmPrefix = platform !== 'win32') { return rmPrefix ? /file:\/\/\//.test(pathURL) ? fileURLToPath(pathURL) : pathURL : pathToFileURL(pathURL).toString() }; global.__dirname = function dirname(pathURL) { return path.dirname(global.__filename(pathURL, true)) }; global.__require = function require(dir = import.meta.url) { return createRequire(dir) }
