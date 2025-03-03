98632a8e74fae6ed0f4eecb2a0db1b5f4c3822db
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-03-03 15:36:51 -0300

    Actualizar config.js

--- config.js
+++ config.js
@@ -56,10 +56,13 @@ global.cen2 = ' ⊹─⭒──✧'
 //global.random1 = getRandomImageBoys
 
 //Imagenes
-global.imagen1 = fs.readFileSync('./storage/logos/Menu1.jpg')
-global.imagen2 = fs.readFileSync('./storage/logos/Menu2.jpg')
-global.imagen3 = fs.readFileSync('./storage/logos/Menu3.jpg')
-global.imagen4 = fs.readFileSync('./storage/logos/Menu4.jpg')
+global.imagen1 = fs.readFileSync('./Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
+global.imagen4 = fs.readFileSync('./Menu.png');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen6= fs.readFileSync('./Menu3.png');
+global.imagen7= fs.readFileSync('./src/hjbot.jpg');
 
 var img = 'https://telegra.ph/file/9a9a4851b638c90ebb214.jpg'; var img1 = 'https://telegra.ph/file/c248943164f6a3d28088c.jpg'; var img2 = 'https://telegra.ph/file/f14102fec3b4104e41e98.jpg'; var img3 = 'https://telegra.ph/file/5646b7540b22e8a00615e.jpg'; var img4 = 'https://telegra.ph/file/e6c11fc60a74a77789f56.jpg'; var img5 = 'https://telegra.ph/file/19af932324e24ceb896cd.jpg'
 var img6 = 'https://telegra.ph/file/e1c9c47c389b17de97a03.jpg'; var img7 = 'https://telegra.ph/file/caa791fa59111b4b4311a.jpg'; var img8 = 'https://telegra.ph/file/76fff527eeaa2b61099ca.jpg'; var img9 = 'https://telegra.ph/file/3dad91ec4eea23ea93f3d.jpg'; var img10 = 'https://telegra.ph/file/4443d4eb77531313b1f9b.jpg'
