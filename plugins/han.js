49b5e1c2f13b9e345c8cda0cccf744b0547c5455
Rayo-ofc <Iv5nfranco24@gmail.com>
2025-03-04 19:35:50 -0300

    test

--- plugins/han.js
+++ plugins/han.js
@@ -3,11 +3,12 @@ const handler = async (m, {conn, usedPrefix}) => {
   if (!db.data.chats[m.chat].audios) return;
   if (!db.data.settings[conn.user.jid].audios_bot && !m.isGroup) return;
   //const s = seconds: '1934.4'
-  let vn = 'https://qu.ax/xbeU.mp3'
+  let vn = 'https://qu.ax/ULCFy.mp3'
   conn.sendPresenceUpdate('recording', m.chat);
   conn.sendMessage(m.chat, {audio: {url: vn}, ptt: true, mimetype: 'audio/mpeg', fileName: `deja de llorar.mp3`}, {quoted: m});
 };
-handler.command = ['o']
+handler.command = ['intro']
 handler.fail = null;
 handler.exp = 100;
+handler.rowner = true;
 export default handler;
